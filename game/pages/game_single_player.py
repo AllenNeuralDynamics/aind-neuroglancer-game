baf917816858d3365ceb4e83bc4178a7b6500a73
Helen Lin <helen.lin@alleninstitute.org>
2025-06-06 16:32:46 -0700

    feat: game sidebar

--- game/pages/game_single_player.py
+++ game/pages/game_single_player.py
@@ -5,10 +5,8 @@ import streamlit as st
 from utils.menu import menu_with_redirect, sanity_check_role
 
 # Redirect to login if not logged in, otherwise show the navigation menu
-menu_with_redirect()
+menu_with_redirect(show_game_menu=True)
 sanity_check_role(f"pages/{Path(__file__).name}")
-
-st.title("Single Player Game")
 
 st.write("This page will allow users to play in single-player mode.")
 st.write("Feature coming soon!")
@@ -16,8 +14,6 @@ st.write("Feature coming soon!")
 # Selected game options
 if "game_options" in st.session_state:
     game_options = st.session_state.game_options
-    # TESTING: display entire game_options dictionary
-    st.write("**Selected Game Options:**", game_options)
 else:
     st.error("No game options selected. Please select a game in the Home page.")
 
